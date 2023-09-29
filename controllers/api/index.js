156d6aa606636081ea43b86399be63f728d29d6d
GreatGaspargoo <b.c.young.97@gmail.com>
2023-09-28 20:41:47 -0600

    added routers for garden and progress routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 
 // /api/users
 const userRoutes = require('./user-routes');
+const gardenRoutes = require('./garden-routes');
+const progressRoutes = require('./progress-routes');
+
 router.use('/users', userRoutes);
+router.use('/garden', gardenRoutes);
+router.use('/progress', progressRoutes);
 
 module.exports = router;
