fdffd49961ff884224abb727dc587a226750cf0a
Brian Whisler <brian.whisler@gmail.com>
2023-09-30 16:14:30 -0600

    linting

--- public/js/addLog.js
+++ public/js/addLog.js
@@ -1,32 +1,38 @@
 const newLogHandler = async (event) => {
-    event.preventDefault();
-    
-    const description = document.querySelector('#describe-input').value.trim();
-    const water_amt = document.querySelector('#water-input').value.trim();
-    const temperature = document.querySelector('#temp-input').value.trim();
-    const humidity = document.querySelector('#humid-input').value.trim();
-    const height = document.querySelector('#height-input').value.trim();
-    const leaf_growth = document.querySelector('#leaf-input').value.trim();
-    const weather = document.querySelector('#weather-input').value.trim();
+  event.preventDefault();
 
-    //note: not all of the fields are required, check which ones are
-    if (description && water_amt && temperature && humidity && height && leaf_growth && weather) {
-        const response = await fetch(`/api/progress`, {
-            method: 'POST',
-            body: JSON.stringify({ description, water_amt, temperature, humidity, height, leaf_growth, weather}),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-
-        if (response.ok) {
-            document.location.replace('/progress');
-        } else {
-            alert('Failed to add log');
-        }
+  const description = document.querySelector('#description-input').value.trim();
+  const water_amt = document.querySelector('#water-input').value.trim();
+  const temperature = document.querySelector('#temperature-input').value.trim();
+  const humidity = document.querySelector('#humidity-input').value.trim();
+  const height = document.querySelector('#height-input').value.trim();
+  const leaf_growth = document.querySelector('#leaf-input').value.trim();
+  const weather = document.querySelector('#weather-input').value.trim();
+  const garden_id = event.target.value;
+  const response = await fetch('/api/progress/' + garden_id, {
+    method: 'POST',
+    body: JSON.stringify({
+      description,
+      water_amt,
+      temperature,
+      humidity,
+      height,
+      leaf_growth,
+      weather,
+      garden_id
+    }),
+    headers: {
+      'Content-Type': 'application/json'
     }
+  });
+
+  if (response.ok) {
+    document.location.replace('/progress/' + garden_id);
+  } else {
+    alert('Failed to add log');
+  }
 };
 
 document
-    .querySelector('.new-log-form')
-    .addEventListener('submit', newLogHandler);
+  .querySelector('#submit-progress')
+  .addEventListener('click', newLogHandler);
