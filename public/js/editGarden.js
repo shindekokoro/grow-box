fffa076041a7a00cc119b3be3d178d5941f57439
Brian Whisler <brian.whisler@gmail.com>
2023-09-29 20:48:55 -0600

    editgarden handler fixes

--- public/js/editGarden.js
+++ public/js/editGarden.js
@@ -1,48 +1,53 @@
+// POST a new plant to /api/garden route
 const newPlantHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const plantName = document.querySelector('#plant-input').value.trim();
+  const plantName = document.querySelector('#plant-input').value.trim();
 
-    if (plantName) {
-        const response = await fetch(`/api/garden`, {
-            method: 'POST',
-            body: JSON.stringify(plantName),
-            headers: {
-                'Content-Type': 'appilcation/json',
-            },
-        });
+  if (plantName) {
+    const response = await fetch('/api/garden', {
+      method: 'POST',
+      body: JSON.stringify({
+        name: plantName
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
 
-        if (response.ok) {
-            document.location.replace('/garden');
-        } else {
-            alert('Failed to add plant');
-        }
+    if (response.ok) {
+      document.location.replace('/garden');
+    } else {
+      alert('Failed to add plant');
     }
+  }
 };
-
-//currently this^ is not creating an entry in the table. possibly to do with user_id?
 
 const delButtonHandler = async (event) => {
-    if (event.target.hasAttribute('plant-id')) {
-        const id = event.target.getAttribute('data-id');
+  if (event.target) {
+    console.log(event);
+    const id = event.target.getAttribute('data-id');
+    console.log(id);
+    const response = await fetch(`/api/garden/${id}`, {
+      method: 'DELETE'
+    });
 
-        const response = await fetch(`/api/garden/${id}`, {
-            method: 'DELETE',
-        });
-
-        if (response.ok) {
-            document.location.replace('/profile');
-        } else {
-            alert('Failed to destroy plant');
-        }
+    if (response.ok) {
+      document.location.replace('/garden');
+    } else {
+      alert('Failed to destroy plant');
     }
+  }
 };
 
 document
-    .querySelector('.new-plant-form')
-    .addEventListener('submit', newPlantHandler);
+  .querySelector('.new-plant-form')
+  .addEventListener('submit', newPlantHandler);
 
-document
-    .querySelector('.plant-list')
-    .addEventListener('click', delButtonHandler);
-//add delete button to plant list
+let deleteButtons = document.querySelectorAll('.delete-button');
+if (deleteButtons) {
+  deleteButtons.forEach((button) => {
+    console.log(button);
+    button.addEventListener('click', delButtonHandler);
+  });
+}
