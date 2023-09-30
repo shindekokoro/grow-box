9d1552d285148c6a2d468c0d8eb2f64f17cb36dd
Brian Whisler <brian.whisler@gmail.com>
2023-09-30 12:02:56 -0600

    more seed data for visual

--- seeds/progressData.js
+++ seeds/progressData.js
@@ -24,6 +24,30 @@ const progressData = [
     weather: 'Cloudy',
     plant_id: 1,
     user_id: 1
+  },
+  {
+    id: 3,
+    description: 'Not Heinz, must be huntz',
+    water_amt: 8,
+    temperature: 68.5,
+    humidity: 40,
+    height: 0,
+    leaf_growth: null,
+    weather: 'Sunny',
+    plant_id: 1,
+    user_id: 1
+  },
+  {
+    id: 4,
+    description: 'Still moist',
+    water_amt: 1,
+    temperature: 69.5,
+    humidity: 90,
+    height: 1,
+    leaf_growth: null,
+    weather: 'Cloudy',
+    plant_id: 1,
+    user_id: 1
   }
 ];
 
