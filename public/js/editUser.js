802ee115412f4463a3bf309c67422a51b0e9d6a3
GreatGaspargoo <b.c.young.97@gmail.com>
2023-10-02 19:27:56 -0600

    consolidated editUser and editPass into 1 function

--- public/js/editUser.js
+++ public/js/editUser.js
@@ -4,40 +4,21 @@ const editUserHandler = async (event) => {
     event.preventDefault();
 
     const username = document.querySelector('#edit-user').value.trim();
-
-    if (username) {
-        const response = await fetch('/api/users/:id', {
-            method: 'PUT',
-            body: JSON.stringify({ username }),
-            headers: {'Content-Type': 'application/json'}
-        });
-
-        if (response.ok) {
-            document.location.replace('/user');
-            alert('Username changed!');
-        } else {
-            alert('Failed to change username.');
-        }
-    }
-};
-
-const editPasswordHandler = async (event) => {
-    event.preventDefault();
-
     const password = document.querySelector('#edit-pass').value.trim();
-
-    if (password) {
-        const response = await fetch('/api/users/:id', {
+    const userID = document.querySelector('#submit-btn').value;
+    
+    if (username || password) {
+        const response = await fetch('/api/users/' + userID, {
             method: 'PUT',
-            body: JSON.stringify({ password }),
+            body: JSON.stringify({ username, password }),
             headers: {'Content-Type' : 'applications/json'}
         });
 
         if (response.ok) {
             document.location.replace('/user');
-            alert('Password changed!');
+            alert('User updated!');
         } else {
-            alert('Failed to change password.')
+            alert('Failed to update user.')
         }
     }
 };
@@ -46,8 +27,5 @@ document
     .querySelector('.edit-user-form')
     .addEventListener('submit', editUserHandler);
 
-document
-    .querySelector('.edit-pass-form')
-    .addEventListener('submit', editPasswordHandler);
 
 //add user page route in controllers
